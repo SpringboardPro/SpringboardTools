fd2283260b73ac4539020036806c2a102f065b6e
Joel Williams <joel.williams@springboard.pro>
2021-10-12 11:54:31 +0100

    Handles error when upissuing an open file or released draft
    Catches the error thrown when trying to rename an open file and displays
    an error message to the user requesting they close the file and try
    again. Previously the script would exit before reaching an input command
    so the user received no feedback on why the script failed.
    Also checks to see whether a draft file has been released when upissuing
    as draft, and throws an error if so. I.e. when trying to
    "upissue - Draft" a revA2 doc after revA has been released the script
     will notify the user that the draft has been released and exit.

--- upissue.py
+++ upissue.py
@@ -1,60 +1,70 @@
-import sys;
-import os;
-import re;
-import shutil;
+import sys
+import os
+import re
+import shutil
 
-if len(sys.argv) == 3 :
+if len(sys.argv) == 3:
     split_path = os.path.split(sys.argv[1])
     upissue = sys.argv[2]
     old_file = split_path[1]
     print("Old Filename: " + old_file)
     path = split_path[0]
-    old_file_parts = re.split(r'([A-Z]{3})-([0-9]{5})-rev([A-Z])([0-9]*) ',old_file)
+    old_file_parts = re.split(r'([A-Z]{3})-([0-9]{5})-rev([A-Z])([0-9]*) ', old_file)
 
-    if old_file_parts[4] == '' :
+    if old_file_parts[4] == '':
         old_file_parts[4] = '0'
-    
-    if len(old_file_parts) == 6 :
-        if upissue in "Draft" :
-            print ("Draft Version upissue")
-            if (old_file_parts[4] == '0') :
-                if (old_file_parts[3] == 'Z') :
+
+    if len(old_file_parts) == 6:
+        if upissue in "Draft":
+            print("Draft Version upissue")
+            if old_file_parts[4] == '0':
+                if old_file_parts[3] == 'Z':
                     print("Error: Existing version is at Z. Sorry but I've not been told where we go after Z!")
                     input()
                     exit()
-                major_version = chr(ord(old_file_parts[3])+1)
+                major_version = chr(ord(old_file_parts[3]) + 1)
                 minor_version = 1
-            else :
+            else:
                 major_version = old_file_parts[3]
-                minor_version = int(old_file_parts[4])+1
-            new_file = "%s-%s-rev%s%d %s" % (old_file_parts[1],old_file_parts[2],major_version,minor_version,old_file_parts[5])
+                minor_version = int(old_file_parts[4]) + 1
+            new_file = "%s-%s-rev%s%d %s" % (
+                old_file_parts[1], old_file_parts[2], major_version, minor_version, old_file_parts[5])
             print("New Filename: " + new_file)
-            if os.path.isfile(path + "/" + new_file) == 0 :
+            release_file = "%s-%s-rev%s %s" % (
+                old_file_parts[1], old_file_parts[2], major_version, old_file_parts[5])
+            if os.path.isfile(path + "/" + new_file) is True:
+                print("Error: New file already exists")
+            elif os.path.isfile(path + "/" + release_file) is True:
+                print("Error: This draft has already been released")
+            else:
                 shutil.copy(path + "/" + old_file, path + "/" + new_file)
                 print("File copied OK")
-            else :
-                print("Error: New file already exists")
-        elif upissue in "Release" :
-            print ("Release Version upissue")
-            if (old_file_parts[4] == '0') :
+        elif upissue in "Release":
+            print("Release Version upissue")
+            if old_file_parts[4] == '0':
                 print("Error: This is already a released document")
                 input()
                 exit()
-            new_file = "%s-%s-rev%s %s" % (old_file_parts[1],old_file_parts[2],old_file_parts[3],old_file_parts[5])
+            new_file = "%s-%s-rev%s %s" % (old_file_parts[1], old_file_parts[2], old_file_parts[3], old_file_parts[5])
             print("New Filename: " + new_file)
-            if os.path.isfile(path + "/" + new_file) == 0 :
-                os.rename(path + "/" + old_file, path + "/" + new_file)
-                print("File renamed OK")
-            else :
+            if os.path.isfile(path + "/" + new_file) == 0:
+                try:
+                    os.rename(path + "/" + old_file, path + "/" + new_file)
+                    print("File renamed OK")
+                except PermissionError:
+                    print("Error: File is open. Please close the file and try again.")
+                    input()
+                    exit()
+            else:
                 print("Error: New file already exists")
-        else :
-            print ("Error: Upissue method undefined")
+        else:
+            print("Error: Upissue method undefined")
             input()
             exit()
-    else :
+    else:
         print("Error: Filename was not as expected")
 
-else :
-    print("Error: Incorrect number of arguements passed in")
+else:
+    print("Error: Incorrect number of arguments passed in")
 
 input()
